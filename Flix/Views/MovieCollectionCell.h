d9fe54f29ba3bb53d0ce2b5a3286861bdd47c4c6
Mohamed Ally Iddi <mudi@fb.com>
2019-06-27 15:27:39 -0700

    added movie collection grid layout

---
+++ Flix/Views/MovieCollectionCell.h
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionCell.h
+//  Flix
+//
+//  Created by mudi on 6/27/19.
+//  Copyright © 2019 mudi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCollectionCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
